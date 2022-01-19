132ab503364adbf1f5975b18e373dde97f5c74e5
SURA126 <nikitasurovagin91@gmail.com>
2022-01-19 19:27:04 +0300

    debug #1

--- todoApp.js
+++ todoApp.js
@@ -1,6 +1,6 @@
 let reqvest = new XMLHttpRequest();
 reqvest.addEventListener("load", ()=> {
-console.log (his.responseText);
+console.log (this.responseText);
 });
 reqvest.open("GET","todo.xml",true);
 reqvest.send(null);
