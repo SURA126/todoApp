b7610aee0d2565c7001997b518fc60cc1a655284
SURA126 <nikitasurovagin91@gmail.com>
2022-01-19 19:23:35 +0300

    parse xml

--- todoApp.js
+++ todoApp.js
@@ -4,3 +4,24 @@ console.log (his.responseText);
 });
 reqvest.open("GET","todo.xml",true);
 reqvest.send(null);
+
+reqvest.onreadystatechange = ()=> {
+    if(this.readyState===4 && this.status===200){
+        showResult(this.responseXML);
+
+
+    }
+
+}
+
+function showResult(xml){
+let text = "";
+path = "//task/title";
+let nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+let result = nodes.iterateNext();
+while(result){
+    text +=`<li>${result.childNodes[0].nodeWalue}</li>`;
+    result = nodes.iterateNext();
+} 
+document.getElementById("tasks").innerHTML=text;
+}
