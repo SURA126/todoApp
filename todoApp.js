585cf7b080699605728141f1d516ea29ae55d5e6
SURA126 <nikitasurovagin91@gmail.com>
2022-01-24 19:52:00 +0300

    fix mistake

--- todoApp.js
+++ todoApp.js
@@ -5,6 +5,8 @@ let nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
 let result = nodes.iterateNext();
 while(result){
         text +=`<li>${result.childNodes[0].nodeValue}</li>`;
+        console.log(result.childNodes[0]);
+        console.log(result.childNodes);
         result = nodes.iterateNext();
 } 
 document.getElementById("tasks").innerHTML=text;
