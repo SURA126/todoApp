be0b11f00607c3a6c41251aaf928ec6d8b5200d9
SURA126 <nikitasurovagin91@gmail.com>
2022-01-24 19:24:19 +0300

    fix mistake

--- todoApp.js
+++ todoApp.js
@@ -1,27 +1,24 @@
+function showResult(xml){
+    let text = "";
+path = "//task/title";
+let nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+let result = nodes.iterateNext();
+while(result){
+        text +=`<li>${result.childNodes[0].nodeWalue}</li>`;
+        result = nodes.iterateNext();
+} 
+document.getElementById("tasks").innerHTML=text;
+}
+
 let reqvest = new XMLHttpRequest();
 /*reqvest.addEventListener("load", ()=> {
 console.log (this.responseText);
 }); */
 reqvest.open("GET","todo.xml",true);
-reqvest.send(null);
+reqvest.send();
 
-reqvest.onreadystatechange = ()=> {
+reqvest.onreadystatechange = function() {
     if(this.readyState===4 && this.status===200){
         showResult(this.responseXML);
-
-
-    }
-
 }
-
-function showResult(xml){
-let text = "";
-path = "//task/title";
-let nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
-let result = nodes.iterateNext();
-while(result){
-    text +=`<li>${result.childNodes[0].nodeWalue}</li>`;
-    result = nodes.iterateNext();
-} 
-document.getElementById("tasks").innerHTML=text;
 }
